f4fe99d002254c74e5e87aec59bd3f0d870a2637
khantuevao <khantuevao@gmail.com>
2023-04-20 13:23:22 +0600

    Add filter animation selection

--- script.js
+++ script.js
@@ -11,3 +11,45 @@ let elements = document.querySelectorAll('.element-animation');
 for (let elm of elements) {
   observer.observe(elm);
 }
+
+let age = document.querySelectorAll('.age')
+for (let i = 0; i < age.length; i++) {
+  age[i].addEventListener('click', () => {
+    if (age[i].classList.contains('selected')) {
+      age[i].classList.remove('selected')
+    } else {
+      for (let j = 0; j < age.length; j++) {
+        age[j].classList.remove('selected')
+      }
+      age[i].classList.add('selected')
+    }
+  })
+}
+
+let year = document.querySelectorAll('.year')
+for (let i = 0; i < year.length; i++) {
+  year[i].addEventListener('click', () => {
+    if (year[i].classList.contains('selected')) {
+      year[i].classList.remove('selected')
+    } else {
+      for (let j = 0; j < year.length; j++) {
+        year[j].classList.remove('selected')
+      }
+      year[i].classList.add('selected')
+    }
+  })
+}
+
+let pub = document.querySelectorAll('.pub')
+for (let i = 0; i < pub.length; i++) {
+  pub[i].addEventListener('click', () => {
+    if (pub[i].classList.contains('selected')) {
+      pub[i].classList.remove('selected')
+    } else {
+      for (let j = 0; j < pub.length; j++) {
+        pub[j].classList.remove('selected')
+      }
+      pub[i].classList.add('selected')
+    }
+  })
+}
